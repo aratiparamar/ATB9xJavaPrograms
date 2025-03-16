2754e63dd3173ff00de40a2e28e3718f737ab9cd
Aarti Parmar <arati.parmar39@gmail.com>
2025-03-16 10:16:57 +0530

    Added all files 14 Oct

---
+++ src/Oct/ex_14102024_operatorsP2/Lab051.java
@@ -0,0 +1,10 @@
+package Oct.ex_14102024_operatorsP2;
+
+public class Lab051 {
+    public static void main(String[] args) {
+        int age=10;
+        //System.out.println(age);
+        boolean is_married= false;
+        //System.out.println(is_married);
+    }
+}
